0fe52224be9870243d09caccb287195ad29d79cd
Paula <paulalorenagui@gmail.com>
2026-06-29 15:51:15 +0300

    Memory Card Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [clickedIds, setClickedIds] = useState([]);
+  const [pics, setPics] = useState([]);
+  useEffect(() => {
+    async function fetchPics() {
+      const response = await fetch(
+        "https://picsum.photos/v2/list?page=3&limit=8",
+      );
+
+      const data = await response.json();
+      setPics(data);
+    }
+
+    fetchPics();
+  }, []);
+  function handleClick(id) {
+    if (clickedIds.includes(id)) {
+      setCurrentScore(0);
+      setClickedIds([]);
+    } else {
+      setClickedIds([...clickedIds, id]);
+      setCurrentScore(currentScore + 1);
+      if (currentScore + 1 > bestScore) {
+        setBestScore(currentScore + 1);
+      }
+    }
+    setPics([...pics].sort(() => Math.random() - 0.5));
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+      <h1>Memory Card Game</h1>
+      <div className="container">
+        {pics.map((img) => (
+          <div className="IMG" key={img.id} onClick={() => handleClick(img.id)}>
+            <img src={img.download_url} alt={img.author} width="200" />
+          </div>
+        ))}
+      </div>
+      <div>
         <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => {
+            setCurrentScore(0);
+            setBestScore(0);
+            setClickedIds([]);
+            setPics([...pics].sort(() => Math.random() - 0.5));
+          }}
         >
-          Count is {count}
+          Restart Game!
         </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
+      <p>Score: {currentScore}</p>
+      <p>Best Score: {bestScore}</p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
